94a9185060a6ba4071c3445578de725ab24fee1b
Andrew Laidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2022-11-18 10:32:23 +0000

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "https://raw.githubusercontent.com/andrewlaidlaw/node-red-sales-manual/bond-girls/bond-girls/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
